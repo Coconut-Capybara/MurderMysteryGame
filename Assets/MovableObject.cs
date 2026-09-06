adcaaa6910e0413512ac466c9faae46b97ea1699
AvianSteel <bwelch1@mail.bradley.edu>
2026-09-06 18:36:10 -0500

    wont work

---
+++ Assets/MovableObject.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class MovableObject : MonoBehaviour
+{
+    [SerializeField] public SplineContainer leftSpline;
+    [SerializeField] public SplineContainer rightSpline;
+    [SerializeField] public int locationIndexGlobal;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -1,5 +1,6 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Splines;
 
 public class MovementController : MonoBehaviour
@@ -10,18 +11,57 @@ public class MovementController : MonoBehaviour
     [SerializeField] private CinemachineSplineDolly splineDolly;
     [SerializeField] private SplineContainer currentSpline;
     [SerializeField] private SplineSettings m_SplineSettings;
+    [SerializeField] private MovableObject movableObject;
+    [SerializeField] private GameObject[] gameObjects;
+    public int localLocationIndex;
     public SplineContainer nextSpline;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        m_SplineSettings = dollySpline.GetComponent<SplineSettings>();
+        //m_SplineSettings = dollySpline.GetComponent<SplineSettings>();
         currentSpline = m_SplineSettings.Spline;
+        localLocationIndex = 0;
+    }
+
+    private void InteractRay()
+    {
+        Vector3 cursorPos = Mouse.current.position.ReadValue();
+        Ray ray = m_Camera.ScreenPointToRay(cursorPos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.GetComponent<MovableObject>() != null)
+            {
+                if ((localLocationIndex > hit.collider.gameObject.GetComponent<MovableObject>().locationIndexGlobal)
+                    && (hit.collider.gameObject.GetComponent<MovableObject>().locationIndexGlobal == 0))
+                {
+                    nextSpline = hit.collider.gameObject.GetComponent<MovableObject>().leftSpline;
+                    localLocationIndex = hit.collider.gameObject.GetComponent<MovableObject>().locationIndexGlobal;
+                }
+                else if (localLocationIndex < hit.collider.gameObject.GetComponent<MovableObject>().locationIndexGlobal)
+                {
+                    nextSpline = hit.collider.gameObject.GetComponent<MovableObject>().rightSpline;
+                    localLocationIndex = hit.collider.gameObject.GetComponent<MovableObject>().locationIndexGlobal;
+                }
+                else
+                {
+                    nextSpline = hit.collider.gameObject.GetComponent<MovableObject>().leftSpline;
+                    localLocationIndex = hit.collider.gameObject.GetComponent<MovableObject>().locationIndexGlobal;
+                }
+                    
+            }
+            else
+            {
+                print("Not assigned");
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        InteractRay();
         if (nextSpline != null)
         {
             currentSpline = nextSpline;
