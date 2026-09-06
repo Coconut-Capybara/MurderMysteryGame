2a9c040a61b7927b823d5c94438e2f3a318d8998
AvianSteel <bwelch1@mail.bradley.edu>
2026-09-05 19:05:34 -0500

    Step1MovementUnfinished

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -1,23 +1,30 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class MovementController : MonoBehaviour
 {
-
+    [SerializeField] private SplineContainer dollySpline;
     [SerializeField] private Camera m_Camera;
     [SerializeField] private CinemachineCamera m_CinemachineCamera;
     [SerializeField] private CinemachineSplineDolly splineDolly;
-    [SerializeField] private int knotIndex;
+    [SerializeField] private SplineContainer currentSpline;
+    [SerializeField] private SplineSettings m_SplineSettings;
+    public SplineContainer nextSpline;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        splineDolly = FindAnyObjectByType<CinemachineCamera>().GetComponent<CinemachineSplineDolly>();
+        m_SplineSettings = dollySpline.GetComponent<SplineSettings>();
+        currentSpline = m_SplineSettings.Spline;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (nextSpline != null)
+        {
+            currentSpline = nextSpline;
+        }
     }
 }
