145d352cfe2f0637b38b7c37deb1888e711caf20
AvianSteel <bwelch1@mail.bradley.edu>
2026-09-09 13:50:46 -0500

    setup

---
+++ Assets/ScriptsG/MovementController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private Transform lookAtTarget;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
