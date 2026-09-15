346c95671e34057a2f641f377bdd4912e692cb1d
AvianSteel <bwelch1@mail.bradley.edu>
2026-09-15 18:41:50 -0500

    FinalTestShennanigans

--- Assets/ScriptsG/TimeController.cs
+++ Assets/ScriptsG/TimeController.cs
@@ -1,17 +1,46 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using static UnityEngine.Rendering.DebugUI;
 
 public class TimeController : MonoBehaviour
 {
+    [Header("Time Variables")]
     [SerializeField] private int timeLeft;
     [SerializeField] private int hoursLeft;
     [SerializeField] private int minutesLeft;
+
+    [Header("Time UI Elements")]
+    [SerializeField] private GameObject timePanel;
+    [SerializeField] private TMP_Text timeAwayText;
+    [SerializeField] private UnityEngine.UI.Button yesButton;
+    [SerializeField] private UnityEngine.UI.Button noButton;
+
+    private int tempTime;
+
+    [Header("Test Variables")]
     public int timeTestAway;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        TimeAway(timeTestAway);
+        //TimeAway(timeTestAway);
+        timePanel.SetActive(false);
     }
 
+    public void PassTimePanel(int timeUse)
+    {
+        timePanel.gameObject.SetActive(true);
+        timeAwayText.text = ("time taken away is: " + timeUse + " minutes" + "\n"  + "You will have " + 
+            ((timeLeft-timeUse) / 60) + " hours and " + ((timeLeft-timeUse) % 60) + " minutes left.");
+        tempTime = timeUse;
+    }
+
+    public void YesButton()
+    {
+        TimeAway(tempTime);
+        timePanel.SetActive(false);
+    }
     public void TimeAway(int time)
     {
         timeLeft = timeLeft - time;
