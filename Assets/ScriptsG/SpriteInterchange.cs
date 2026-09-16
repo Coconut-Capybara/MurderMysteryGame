9b7c18f598898595c07744f686e524a19fbdef62
AvianSteel <bwelch1@mail.bradley.edu>
2026-09-16 17:09:42 -0500

    Object Highlighting Script working

--- Assets/ScriptsG/SpriteInterchange.cs
+++ Assets/ScriptsG/SpriteInterchange.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SpriteInterchange : MonoBehaviour
 {
+    [Header("Object Properties Script")]
+    [SerializeField] private ObjectProperties objectProperties;
+
+    [SerializeField] private GameObject thisObject;
+
+    private PlayerInteract player;
+    private SpriteRenderer spriteRenderer;
+
+
+    [SerializeField] private Camera playerCam;
+
+
+    [Header("Play Mode View Variables")]
+    public Sprite originalSprite;
+    public Sprite hoverSprite;
+    public Sprite altSprite1;
+    public Sprite altSprite2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindFirstObjectByType<PlayerInteract>();
+        originalSprite = objectProperties.itemSprite;
+        hoverSprite = objectProperties.highlightSprite;
+        spriteRenderer = thisObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = originalSprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 cursorPos = Mouse.current.position.ReadValue();
+        Ray ray = playerCam.ScreenPointToRay(cursorPos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.GetComponent<SpriteInterchange>() != null)
+            {
+                spriteRenderer.sprite = hoverSprite;
+            }
+        }
+        else
+        {
+            spriteRenderer.sprite = originalSprite;   
+        }
     }
 }
